705da211bda30f4babe76a040c4baf1e4298747d
Rosa-M-FS <rmlopezrobles@gmail.com>
2025-03-09 18:28:12 +0100

    Ejercicio Terminado (form)

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,28 @@ const[newName,setNewName]=useState('');
 
 const teachers=['Data','Reyes','Yolanda'];
 
-const changeName=(name)=>{
-    setName(name);
+const changeName=()=>{
+    if(newName.trim())
+    setName(newName);//cambio el nombre de name con el newname
+    setNewName('');
 };
-
+/* onsubmit = action="/nuevonombre Method= post" se gestiona solo desde react */
+/*preventDefault evita que el form se recargue */
+/**onchange se dispara cada vez que se escribe o borra algo(cada vez que el campo de entrada cambia) */
 return (
     <div>
         <h2>Teacher Name: {name}</h2>
+        
+        <form onSubmit={(event) =>{
+            event.preventDefault(); changeName();
+        }}>
+            <input type="text" name="newName" id="newName" value={newName} placeholder='add name' onChange={(event)=>setNewName(event.target.value)}/>
+            <button type='submit'>Add</button>
+        </form>
+
         <ul>
             {teachers.map((teacher,ind)=>(
-                <li key={ind} onClick={()=>changeName(teacher)}>
+                <li key={ind} onClick={()=>setName(teacher)}>
                     {teacher}
                 </li>
             ))}
