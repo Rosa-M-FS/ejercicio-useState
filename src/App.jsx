72aca7d771c0521f721e9b09e8fbdcbe68c1ca10
Rosa-M-FS <rmlopezrobles@gmail.com>
2025-03-09 17:21:07 +0100

    Primera parte terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
- //Aquí tu código
+//Aquí tu código
+const[name,setName]=useState('Sofia'); //variables de estado name = Var setName = funcion que cambia valor de name
+const[newName,setNewName]=useState('');
+
+const teachers=['Data','Reyes','Yolanda'];
+
+const changeName=(name)=>{
+    setName(name);
+};
+
+return (
+    <div>
+        <h2>Teacher Name: {name}</h2>
+        <ul>
+            {teachers.map((teacher,ind)=>(
+                <li key={ind} onClick={()=>changeName(teacher)}>
+                    {teacher}
+                </li>
+            ))}
+        </ul>
+    </div>
+);
+//react necesita la key para identificar cada li 
 }
 
-export default App
+export default App;
